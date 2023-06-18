975d960b69d253228ecf7c0e9e95dc5092b80b21
Spike <lukasz.piwowar1@protonmail.com>
2023-06-18 20:24:44 +0200

    added: clearing canvas, color picking, drawing with click+drag

--- script.js
+++ script.js
@@ -7,18 +7,57 @@ function createCanvas(sideCount) {
     pixel.style.width = `${400/sideCount}px`;
     canvas.appendChild(pixel);
   }
+  return document.querySelectorAll('.pixel');
 }
 
-function setDraw(color) {
-  const pixels = document.querySelectorAll('.pixel');
+function setDraw(pixels) {
+  let color = 'black';
+  let isDrawing = false;
+  const draw = (e) => {
+    e.target.style.backgroundColor = color;
+  };
+
+  const mouseDown = (e) => {
+    isDrawing = true;
+    if(isDrawing) draw(e);
+  }
+  const mouseUp = () => {
+    isDrawing = false;
+  }
+  const mouseMoving = (e) => {
+    if(isDrawing) draw(e);
+  }
+
   pixels.forEach(px => {
-    px.addEventListener('click', (e) => {
-      e.target.style.backgroundColor = color;
-    });
+    px.addEventListener('mousedown', mouseDown);
+    px.addEventListener('mouseup', mouseUp);
+    px.addEventListener('mousemove', mouseMoving);
   });
+
+  const setColor = (newColor) => {
+    color = newColor;
+  };
+
+  return {
+    setColor: setColor
+  };
+}
+
+function clearCanvas(pixels) {
+  pixels.forEach(px => {
+    px.style.backgroundColor = 'transparent';
+  })
 }
 
 const canvas = document.querySelector('.canvas');
+const pixels = createCanvas(40);
+const clearButton = document.querySelector('.clear-button');
+const colorPicker = document.querySelector('.color-picker');
 
-createCanvas(10);
-setDraw('red');
+
+clearButton.addEventListener('click', () => {clearCanvas(pixels)});
+const draw = setDraw(pixels);
+
+colorPicker.addEventListener('change', (e) => {
+  draw.setColor(e.target.value);
+});
